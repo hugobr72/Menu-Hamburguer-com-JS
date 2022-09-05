e9ca57e2cf5603b92d2879325cea8cbc60537cb3
Hugo Barbosa Rodrigues <hugobrod72@gmail.com>
2022-09-05 20:00:06 -0300

    Adicionando o X

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const botao = document.getElementById('btn-mobile')
 function menu(){
     const nav = document.getElementById('nav')
     const burguer =document.getElementById('hamburguer')
+    const icon = document.getElementsByTagName('i')[0]
+    const iconX = document.getElementsByTagName('i')[1]
     nav.classList.toggle('active')
     burguer.classList.toggle('active')
-    
+    icon.classList.toggle('active')
+    iconX.classList.toggle('active')
 }
 
 botao.addEventListener('click',menu)
